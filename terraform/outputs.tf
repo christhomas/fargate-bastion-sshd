e14877ff8ddbc046b526db8b9d6c590263851ec8
Chris Thomas <chris.thomas@antimatter-studios.com>
2020-04-11 12:46:43 +0200

    Fix the outputs to test how many elements are there instead of using count attribute

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,17 +1,11 @@
 output "security_group_id" {
-  count = "${var.enable_bastion == 1 ? 1 : 0}"
-
-  value = "${aws_security_group.load_balancer[0].id}"
+  value = "${length(aws_security_group.load_balancer[0]) == 1 ? aws_security_group.load_balancer[0].id : "disabled"}"
 }
 
 output "nlb_dns_name" {
-  count = "${var.enable_bastion == 1 ? 1 : 0}"
-
-  value = "${aws_lb.bastion[0].dns_name}"
+  value = "${length(aws_lb.bastion[0]) == 1 ? aws_lb.bastion[0].dns_name : "disabled"}"
 }
 
 output "port" {
-  count = "${var.enable_bastion == 1 ? 1 : 0}"
-
   value = "${var.port}"
 }
