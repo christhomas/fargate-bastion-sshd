32e6af3f786578ba7cb632a1ff48768fb1765fa9
Chris Thomas <chris.thomas@antimatter-studios.com>
2020-04-24 17:46:32 +0200

    change the output name to a more normal 'host' instead

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,7 +2,7 @@ output "security_group_id" {
   value = length(aws_security_group.bastion) == 1 ? aws_security_group.bastion[0].id : null
 }
 
-output "nlb_dns_name" {
+output "host" {
   value = length(aws_lb.bastion) == 1 ? aws_lb.bastion[0].dns_name : null
 }
 
