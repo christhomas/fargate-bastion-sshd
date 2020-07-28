c19b5e7b45ea8a547e5c3ffb54a597694db1f89a
Chris Thomas <chris.thomas@antimatter-studios.com>
2020-07-28 07:27:27 +0200

    fix a bug where the number of elements is one

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,17 +1,17 @@
 output "security_group_id" {
   value = {
-    for key, value in var.env_list: value => length(aws_security_group.bastion) >= key ? aws_security_group.bastion[key].id : null
+  for key, value in var.env_list: value => length(aws_security_group.bastion) > key ? aws_security_group.bastion[key].id : null
   }
 }
 
 output "host" {
   value = {
-    for key, value in var.env_list: value => length(aws_lb.bastion) >= key ? aws_lb.bastion[key].dns_name : null
+  for key, value in var.env_list: value => length(aws_lb.bastion) > key ? aws_lb.bastion[key].dns_name : null
   }
 }
 
 output "port" {
   value = {
-    for key, value in var.env_list: value => length(aws_security_group.bastion) >= key ? var.port : null
+  for key, value in var.env_list: value => length(aws_security_group.bastion) >= key ? var.port : null
   }
 }
