db41831c59b11a9013e3bc33bb24b1933c86f80e
Chris Thomas <chris.thomas@antimatter-studios.com>
2020-07-28 07:57:57 +0200

    add the cloudwatch arn to the outputs so it can be used to set permissions

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,17 +1,21 @@
 output "security_group_id" {
   value = {
-  for key, value in var.env_list: value => length(aws_security_group.bastion) > key ? aws_security_group.bastion[key].id : null
+    for key, value in var.env_list: value => length(aws_security_group.bastion) > key ? aws_security_group.bastion[key].id : null
   }
 }
 
 output "host" {
   value = {
-  for key, value in var.env_list: value => length(aws_lb.bastion) > key ? aws_lb.bastion[key].dns_name : null
+    for key, value in var.env_list: value => length(aws_lb.bastion) > key ? aws_lb.bastion[key].dns_name : null
   }
 }
 
 output "port" {
   value = {
-  for key, value in var.env_list: value => length(aws_security_group.bastion) >= key ? var.port : null
+    for key, value in var.env_list: value => length(aws_security_group.bastion) >= key ? var.port : null
   }
+}
+
+output "cloudwatch_log_arn" {
+  value = aws_cloudwatch_log_group.bastion.*.arn
 }
